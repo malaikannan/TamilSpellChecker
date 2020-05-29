a7ecde5ef3d16e442df62b1eeb1853655f983d56
Muthiah Annamalai <ezhillang@gmail.com>
2020-05-28 18:09:31 -0700

    Norvig spelling suggestor is added from Open-Tamil solthiruthi. We reduce search time from 2.48s -> 0.032s in case of one word and this is generally true for all searches by virtue of generating edit-distance 2 candidates of incorrectly spelled word; essentially we run 1000s of searches in dictionary as opposed to millions of searches in dictionary in brute force approach.
    Unittests are setup more formally.
    
    Contributions from Ezhil Language Foundation are released under terms of this project (Apache 2.0).

--- test.py
+++ test.py
@@ -1,30 +1,53 @@
 import unittest
-
+import timeit
 from TamilwordChecker import TamilwordChecker
-
-#####To check whether word exist or not using Bloom Filter ############
-unique_word_count = 2043478
-tamilwordchecker = TamilwordChecker(unique_word_count,"tamil_bloom_filter.txt")
-assert tamilwordchecker.tamil_word_exists("மேகம்") == True, "Bloom Filter file is not proper"
-print(tamilwordchecker.tamil_word_exists("மேகம்"))
-
-#####To check whether word exist or not using Bloom Filter ############
-assert tamilwordchecker.tamil_word_exists("hello") == False, "Bloom Filter file is not proper"
-print(tamilwordchecker.tamil_word_exists("hello"))
-
 from TamilSpellingAutoCorrect import TamilSpellingAutoCorrect
-spellchecker  = TamilSpellingAutoCorrect("tamil_bloom_filter.txt","tamilwordlist.txt")
 
-suggested_spell_checker_list = ['மேட்ம்', 'மேகநம்', 'மேகம்', 'மேகாம்', 'மேகும்', 'மேன்ம்']
-from_spell_checker_list = spellchecker.tamil_correct_spelling("மேக்ம்")
-assert set(suggested_spell_checker_list) == set(from_spell_checker_list), "Auto correct is not proper" 
+unique_word_count = 2043478
+timer = timeit.Timer()
 
-suggested_spell_checker_list = []
-from_spell_checker_list = spellchecker.tamil_correct_spelling("மேகம்")
-assert set(suggested_spell_checker_list) == set(from_spell_checker_list), "Auto correct is not proper" 
+#####To check whether word exist or not using Bloom Filter ############
+class TamilSpellerTest(unittest.TestCase):
+    def setUp(self):
+        self.tamilwordchecker = TamilwordChecker(unique_word_count,"tamil_bloom_filter.txt")
+        self.spellchecker  = TamilSpellingAutoCorrect("tamil_bloom_filter.txt","tamilwordlist.txt")
 
+    def test_megam_bloomfilter_dictionary(self):
+        self.assertTrue(self.tamilwordchecker.tamil_word_exists("மேகம்") , "Bloom Filter file is not proper")
+        print(self.tamilwordchecker.tamil_word_exists("மேகம்"))
+        #####To check whether word exist or not using Bloom Filter ############
+        self.assertFalse( self.tamilwordchecker.tamil_word_exists("hello") , "Bloom Filter file is not proper")
+        print(self.tamilwordchecker.tamil_word_exists("hello"))
 
+    def test_autocorrect_dictionary_incorrectword(self):
+        suggested_spell_checker_list = ['மேட்ம்', 'மேகநம்', 'மேகம்', 'மேகாம்', 'மேகும்', 'மேன்ம்']
+        s_time = timer.timer()
+        from_spell_checker_list = self.spellchecker.tamil_correct_spelling("மேக்ம்")
+        print("Full search @/ incorrect word|Time ={0}".format( timer.timer() - s_time) )
+        self.assertListEqual( sorted(suggested_spell_checker_list), sorted(from_spell_checker_list), "Auto correct is not proper")
 
+    def test_autocorrect_dictionary_correctword(self):
+        suggested_spell_checker_list = []
+        s_time = timer.timer()
+        from_spell_checker_list = self.spellchecker.tamil_correct_spelling("மேகம்")
+        print("Full search @ w/ correct word|Time ={0}".format( timer.timer() - s_time ) )
+        self.assertListEqual( sorted(suggested_spell_checker_list), sorted(from_spell_checker_list), "Auto correct is not proper")
 
+    def test_autocorrect_norvig_correctword(self):
+        suggested_word = 'மேகம்'
+        s_time = timer.timer()
+        from_spell_checker_list = self.spellchecker.tamil_Norvig_correct_spelling("மேகம்")
+        print("Norvig @ w/ correct word|Time ={0}".format( timer.timer() - s_time ) )
+        #print(from_spell_checker_list)
+        self.assertTrue(  len(from_spell_checker_list) == 0, "Auto correct is not proper")
 
+    def test_autocorrect_norvig_incorrectword(self):
+        suggested_word = 'மேகம்'
+        s_time = timer.timer()
+        from_spell_checker_list = self.spellchecker.tamil_Norvig_correct_spelling("மேக்ம்")
+        print("Norvig @ w/ incorrect word|Time ={0}".format( timer.timer() - s_time ) )
+        #print(from_spell_checker_list)
+        self.assertIn( suggested_word, from_spell_checker_list, "Auto correct is not proper")
 
+if __name__ == "__main__":
+    unittest.main()
